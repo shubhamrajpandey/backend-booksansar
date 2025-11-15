eaf0fe678b19c140fd3cbdaf6a6a4049676b603d
Shubham Raj Pandey <shubhamrajpandey875@gmail.com>
2025-11-15 22:07:54 +0545

    fix:cors origin

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,14 @@ import dotenv from "dotenv";
 import connectDb from "./config/db";
 import authRoute from "./routes/auth.routes";
 import otpRoute from "./routes/otp.routes";
+import cors from "cors"
+
 const app = express();
+
+app.use(cors({
+  origin:" http://localhost:3000",
+  credentials:true
+}))
 
 dotenv.config();
 
