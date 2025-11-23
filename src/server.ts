1a0dac74cf0cb08502fe1fffb6aa552bf30f0c7b
Shubham Raj Pandey <shubhamrajpandey875@gmail.com>
2025-11-23 21:40:29 +0545

    fix:build error of typescript

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();             
 import connectDb from "./config/db";
 import authRoute from "./routes/auth.routes";
 import otpRoute from "./routes/otp.routes";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 
 app.use(cors({
-  origin: "*",
-  credentials: true
+  origin: "http://localhost:3000",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-
-dotenv.config();
+app.use(express.json());
 
 connectDb();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
-
-app.use('/api/auth',authRoute);
-app.use('/api/otp',otpRoute);
-
+app.use('/api/auth', authRoute);
+app.use('/api/otp', otpRoute);
 
 app.listen(PORT, () =>
   console.log(`Server running on port http://localhost:${PORT}`)
