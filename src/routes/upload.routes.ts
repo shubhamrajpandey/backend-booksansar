2a2f501b68c717d4e8a1f4ce8baaa568a5b2b732
Shubham Raj Pandey <shubhamrajpandey875@gmail.com>
2026-01-03 22:12:06 +0545

    fix:file upload logic

--- src/routes/upload.routes.ts
+++ src/routes/upload.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { upload } from "../middlewares/upload.middleware";
 import { uploadFile } from "../controllers/upload.controller";
-import { authenticateToken } from "../middlewares/auth.middleware";
+
 
 const router = Router();
 
-router.post("/", authenticateToken, upload.single("image"), uploadFile);
+router.post("/", upload.single("image"), uploadFile);
 
 export default router;
